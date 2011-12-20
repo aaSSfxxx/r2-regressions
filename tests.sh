d46f633cce2b5b41fb7bf393ef1917acd79231f2
vext01 <vext01@gmail.com>
2011-12-20 21:11:10 +0000

    tear out broken gdb/valgrind code

--- tests.sh
+++ tests.sh
@@ -1,30 +1,28 @@
-# do not execute #
-. ../config.sh
-
-out=`mktemp out.XXXXXX`
-rad=`mktemp rad.XXXXXX`
-exp=`mktemp exp.XXXXXX`
+#!/do/not/execute
 
 run_test() {
-	cmd="${DBG} ${r2} -e scr.color=0 -n -q -i ${rad} ${ARGS} ${FILE} ${PIP}"
-	echo "TEST: `basename ${NAME}`"
+
+	cmd="echo q | ${r2} -e scr.color=0 -n -q -i ${rad} ${ARGS} ${FILE} > ${out}"
+
+	echo "Next Test: `basename ${NAME}`"
 	echo "Running: ${cmd}"
 
+	# put expected outcome and program to run in files
 	echo "${CMDS}" > ${rad}
 	echo "${EXPECT}" > ${exp}
 
 	eval ${cmd}
 	if [ ! $? = 0 ]; then
 		printf "\x1b[31m"
-		echo "Running ${NAME} ... FAIL (radare2 crashed?)"
+		echo "FAIL (Radare2 crashed?)"
 		printf "\x1b[0m"
 	elif [ "`cat $out`" = "${EXPECT}" ]; then
 		printf "\x1b[32m"
-		echo "Running ${NAME} ... SUCCESS"
+		echo "SUCCESS"
 		printf "\x1b[0m"
 	else
 		printf "\x1b[31m"
-		echo "Running ${NAME} ... FAIL (unexpected outcome)"
+		echo "FAIL (Unexpected outcome)"
 		printf "\x1b[0m"
 		diff -u ${exp} ${out}
 	fi
@@ -32,23 +30,15 @@ run_test() {
 	echo "-------------------------------------------------------------------"
 }
 
-PIP=">${out}"
-case "${DEBUG}" in
-0|no)
-	DBG="cat ${rad} |"
-	DBG="echo q |"
-	;;
-1|yes|gdb)
-	DBG="gdb --args"
-	PIP=""
-	;;
-2|valgrind)
-	DBG="valgrind --leak-check=full --track-origins=yes"
-	DBG="${DBG} --workaround-gcc296-bugs=yes --read-var-info=yes"
-	PIP="2>&1 | tee out.valgrind"
-	;;
-esac
+r2=${R2}
+if [ -z "${R2}" ]; then
+	r2=`which radare2`
+fi
+
+out=`mktemp out.XXXXXX`
+rad=`mktemp rad.XXXXXX`
+exp=`mktemp exp.XXXXXX`
 
 NAME=$0
-export DBG CMDS NAME ARGS FILE EXPECT PIP
+
 run_test
