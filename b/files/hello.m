bbbf46da5a019a1f6b58576679258320514b1d4a
pancake <pancake@nopcode.org>
2012-11-06 09:53:25 +0100

    Add objc binary files

---
+++ b/files/hello.m
@@ -0,0 +1,39 @@
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject {
+	/* instance variables */
+	int num;
+}
+- (void)sayHello:(int)i;
+@end
+
+main() {
+	Test *o = [[Test alloc] init];
+	[o sayHello:3];
+}
+
+@implementation Test
+// _c_Test__callMeNot
+// static method
++ (void)callMeNot {
+	NSLog (@"Hahha");
+}
+
+-(id) init {
+	self = [super init];
+	return self;
+}
+- (void)sayLong:(unsigned long long)lllll {
+	NSLog (@"Hello World ");
+}
+// _i_Test__sayHello__
+- (void)sayHello3:(int)n :(int)q {
+	NSLog (@"Hello World ");
+}
+- (void)sayHello:(int)n {
+	NSLog (@"Hello World ");
+}
+- (void)sayHello2 {
+	NSLog (@"Hello World ");
+}
+@end
